8146c35d2bed2e296abdd5a9d8ce932d27174ba7
akecka <agnkecka@gmail.com>
2017-12-28 20:50:15 +0100

    commit

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ $(document).ready(function() {
   function handleTaskDeleteRequest() {
     var parentEl = $(this).parent().parent();
     var taskId = parentEl.attr('data-task-id');
-    var requestUrl = apiRoot + 'deleteTask';
+    var requestUrl = apiRoot + 'deleteTask/';
 
     $.ajax({
       url: requestUrl + $.param({
